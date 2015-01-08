3c1ad41a115476cc6155d01d3438f7e9e5cd2c19
Andy Booth <andy.booth.uk@googlemail.com>
2015-01-08 16:00:43 +0000

    Initial form and carousel styles

--- Prototypr/prototypr.js
+++ Prototypr/prototypr.js
@@ -1,8 +1,16 @@
 ﻿$(function () {
-    var selector = "main > section";
+    $.ajaxSetup({
+        cache: false
+    });
+
+    function bindSlides() {
+        $("main > section + section").addClass("next");
+    }
+
+    var selector = "main > *";
     var current = $(selector);
 
-    $("body").on("click", "a", function (e) {
+    $("body").on("click", "a[href]", function (e) {
         e.preventDefault();
 
         var anchor = $(this);
@@ -12,31 +20,64 @@
 
         $.get(href, function (data) {
             var html = $("<div></div>").html(data);
-            var next = html.find(selector);
+            var content = html.find(selector);
+            
+            if (content.filter("section").length == 1) {
+                var next = content.first("section");
 
-            next.addClass("next");
+                next.addClass("next");
 
-            current.after(next);
+                current.after(content);
 
-            next.one("transitionend webkitTransitionEnd", function () {
+                next.one("transitionend webkitTransitionEnd", function () {
+                    current.remove();
+
+                    var header = html.find("header");
+
+                    if (header.length > 0) {
+                        $("body > header").html(header);
+                    }
+
+                    var footer = html.find("footer");
+
+                    if (footer.length > 0) {
+                        $("body > footer").html(footer);
+                    }
+
+                    bindSlides();
+                });
+
+                setTimeout(function () {
+                    next.removeClass("next");
+                }, 60);
+            }
+            else {
+                current.after(content);
                 current.remove();
-
-                var header = html.find("header");
-
-                if (header.length > 0) {
-                    $("body > header").html(header);
-                }
-
-                var footer = html.find("footer");
-
-                if (footer.length > 0) {
-                    $("body > footer").html(footer);
-                }
-            });
-
-            setTimeout(function () {
-                next.removeClass("next");
-            }, 60);
+            }
         });
     });
+
+    $("body").on("click", "main > nav > a", function () {
+        var anchor = $(this);
+        var index = anchor.parent().children("a").index(anchor);
+
+        current = $(selector).not(".next").not(".prev");
+        next = $(selector).eq(index);
+
+        var currentIndex = current.parent().children("section").index(current);
+
+        next
+            .removeClass("prev")
+            .removeClass("next");
+
+        if (currentIndex > index) {
+            current.addClass("next");
+        }
+        else {
+            current.addClass("prev");
+        }
+    });
+
+    bindSlides();
 });
